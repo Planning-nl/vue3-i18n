1a5029bafc1315c69cd0fd1de00a84995caa3312
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-11-13 14:39:34 +0100

    chore(externals): webpack build: use external vue package for @vue/reactivity

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,7 @@ module.exports = require("@planning.nl/webpack-config")({
     entry: {
         "planning-i18n": "./src/index.ts",
     },
+    externals: {
+        "@vue/reactivity": { commonjs: "vue", commonjs2: "vue", amd: "vue", root: "Vue" },
+    },
 });

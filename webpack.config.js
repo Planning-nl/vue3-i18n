b838e762d66ed12319f1c34d15e54776c6c12cd5
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-11-17 16:52:09 +0100

    fix(rename)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = require("@planning.nl/webpack-config")({
     entry: {
-        "planning-i18n": "./src/index.ts",
+        "vue3-i18n": "./src/index.ts",
     },
     externals: {
         "@vue/reactivity": { commonjs: "vue", commonjs2: "vue", amd: "vue", root: "Vue" },

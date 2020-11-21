899643234ca939feb92d14b20085bdf9fb7e3361
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-11-21 15:29:34 +0100

    feat(number/datetime/html): export formats and helpers

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { getLocales, locales, withLocales } from "./locales";
 export { patch } from "./patch";
 export { patchLocale } from "./patchLocale";
 export { plural } from "./utils/plural";
-export { number } from "./utils/number";
-export { datetime, dateTimeFormats } from "./utils/datetime";
+export { number, numberParts, numberHtml } from "./utils/number";
+export { datetime, datetimeParts, datetimeHtml, dateTimeFormats } from "./utils/datetime";
+export { escapeHtml } from "./utils/escapeHtml";
